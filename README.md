46d10caa4050ef2a7501f2553fc4ddde59f801c8
Hugh Kennedy <hughskennedy@gmail.com>
2013-07-13 16:21:59 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# continuous-observer #
+
+Take a [continuous ndarray](http://github.com/ndarray-continuous) and, given a
+set of "observable" positions, automatically add and remove chunks as required.
+
+Use this for painless chunk management with one or more visible perspectives.
+
+## Installation ##
+
+``` bash
+npm install continuous-observer
+```
+
+## Usage ##
+
+### `observe = require('continuous-observer')(field[, range[, linger]])` ###
+
+Returns a function that can be used to update the chunks in use, given a
+continuous ndarray (`field`). Optionally:
+
+* `range` is the amount of surrounding chunks to include. Defaults to 1.
+* `linger` is the amount of surrounding chunks to not remove when cleaning up.
+  Defaults to `range`, and cannot be set to below `range`.
+
+### `observe(points)` ###
+
+`points` is an array of arrays, with each array representing an observer's
+position. Can also handle a single array as well, if you're only using one
+observer.
+
+``` javascript
+// Create a continuous ndarray with 32x32 chunks
+var field = require('ndarray-continuous')({ shape: [32, 32] })
+// Create the observer
+var moveTo = require('continuous-observer')(field, 1, 2)
+
+// "Move" the observer to the origin
+moveTo([0, 0])
+
+// Ahead one chunk
+moveTo([32, 32])
+
+// Ahead another chunk - this will remove
+// some of the older chunks.
+moveTo([32, 32])
+
+// Including another observer just involves
+// adding another position to the array.
+moveTo([[32, 32], [0, 0]])
+```
---
+++ index.js
@@ -0,0 +1,65 @@
+var dup = require('dup')
+var map = require('map-async')
+var cells = require('cell-range')
+var round = Math.round
+var min = Math.min
+var max = Math.max
+var abs = Math.abs
+
+function observer(field, range, linger) {
+  range = (+range|0) || 1
+  linger = max((+linger|0) - range, range)
+
+  var shape = field.shape
+  var dims = shape.length
+  var neighbours = cells(dup(dims, -range), dup(dims, range))
+
+  return function observe(places, callback) {
+    if (!Array.isArray(places[0])) places = [places]
+    callback = callback || function(){}
+
+    map(places, function(coord, next) {
+      coord = coord.slice()
+      var i = dims
+      while (i--) coord[i] = round(coord[i] / shape[i])
+
+      map(neighbours, function(offset, next) {
+        var pos = coord.slice()
+        var i = dims
+        while (i--) pos[i] += offset[i]
+        field.chunk(pos, next)
+      }, next)
+    }, function(err) {
+      if (err) return next(err)
+
+      var closest = Infinity
+      var flagged = []
+      var distance = 0
+      var l = places.length
+      var pos
+      var i
+
+      for (var key in field.index) {
+        pos = field.index[key].position
+
+        for (i = 0; i < l; i += 1) {
+          distance = 0
+          for (var d = 0; d < dims; d += 1) {
+            distance += abs(pos[d] - places[i][d] / shape[d])
+          }
+
+          closest = distance < closest
+            ? distance
+            : closest
+        }
+
+        if (closest > range + linger) flagged.push(pos)
+      }
+
+      i = flagged.length
+      while (i--) field.remove(flagged[i])
+
+      return callback(null)
+    })
+  }
+}
